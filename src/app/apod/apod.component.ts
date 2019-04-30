22e4ef332aaa072b92e3504f4fa4dc295cc1a733
Michael Howard <michael.howard.pha@icloud.com>
2019-04-30 09:12:36 -0500

    Display images and text descriptions

--- src/app/apod/apod.component.ts
+++ src/app/apod/apod.component.ts
@@ -1,6 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
-import { ApodService } from '../api/apod.service';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  ActivatedRoute
+} from '@angular/router';
+import {
+  ApodService
+} from '../api/apod.service';
+import {
+  Apod
+} from '../../../models/apod';
 
 @Component({
   selector: 'app-apod',
@@ -9,14 +19,43 @@ import { ApodService } from '../api/apod.service';
 })
 export class ApodComponent implements OnInit {
 
-  constructor(private apodService: ApodService) { }
+  apod: Apod;
+  //1. Create date as an instance variable
+  date: string;
+
+  constructor(
+    private apodService: ApodService,
+    private router: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.apodService.getApod().subscribe(
-      (response:any)=>{
-        console.log(response);
+    this.router.params.subscribe((params) => {
+      this.getApod(params['date']);
+    });
+  }
+
+  getApod(date: string): void {
+
+    this.apodService.getApod(date).subscribe(
+      (response: any) => {
+        this.apod = response;
+        //3.  Update this.date on each API call
+        this.date = this.randomDate(new Date(1995, 5, 16), new Date());
+        //4. Log this.date to the JS console
+        console.log(this.date);
       }
     );
+
+  }
+
+  //2. Create a method that returns a random date
+  randomDate(start, end): string {
+    let date = new Date(
+      start.getTime() + Math.random() *
+      (end.getTime() - start.getTime())
+    );
+
+    return new Date(date).toISOString().slice(0, 10);
   }
 
 }
