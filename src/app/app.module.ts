a76a069149c467e5cbd1acd8194ff54cc8bf2474
Michael Howard <michael.howard.pha@icloud.com>
2019-05-12 20:58:20 -0500

    Import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  BrowserModule
+} from '@angular/platform-browser';
+import {
+  NgModule
+} from '@angular/core';
+// 1. Import the app module
+import {
+  HttpClientModule
+} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ApodComponent } from './apod/apod.component';
-
-//Import the pipe from node_modules
-import { SafePipeModule } from 'safe-pipe';
-
-import { NgApodConfig } from '../../config/ng-apod.config';
-import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './logout/logout.component';
-import { RegisterComponent } from './register/register.component';
+import {
+  AppRoutingModule
+} from './app-routing.module';
+import {
+  AppComponent
+} from './app.component';
+import {
+  LoginComponent
+} from './login/login.component';
+import {
+  LogoutComponent
+} from './logout/logout.component';
+import {
+  RegisterComponent
+} from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApodComponent,
     LoginComponent,
     LogoutComponent,
     RegisterComponent
@@ -25,12 +35,10 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    SafePipeModule //Add the pipe to your list of imports
+    // 2. Add the module to the list of imports.
+    HttpClientModule
   ],
-  providers: [
-    NgApodConfig
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
